b255bede2459b187e72313333bb1f9f318ae3a15
Umar Tasuiev <prmtzn@icloud.com>
2023-11-12 01:30:46 +0200

    DZ4Tasuiev.py

--- DZ4Tasuiev.py
+++ DZ4Tasuiev.py
@@ -1,8 +1,17 @@
-# Зчитування фрази з клавіатури
+#Перший варіант вирішення задачі
+
 input_phrase = input("Введіть фразу: ")
 
-# Визначення довжини введеної фрази
 length_of_phrase = len(input_phrase)
 
-# Виведення результату
 print(f"Фраза '{input_phrase}' має довжину {length_of_phrase} символів.")
+
+#Другий варіает вирішення задачі
+
+input_phrase = input("Введіть фразу: ")
+
+length_of_phrase = len(input_phrase)
+
+result_string = "має" if length_of_phrase > 0 else "не має"
+
+print("Фраза '{}' {} довжину {} символів.".format(input_phrase, result_string, length_of_phrase))
