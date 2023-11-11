5197cc2a6561ebd59870761eb6bd7f525c5602ed
Umar Tasuiev <prmtzn@icloud.com>
2023-11-12 01:59:01 +0200

    EXPERIMRNTAL.py

--- EXPERIMENTAL.py
+++ EXPERIMENTAL.py
@@ -1,15 +1,15 @@
-true = True
-false = False
-age_str = input("Скільки вам років?")
-age = int(age_str)
-if age >= 18:
-    print("Ви повнолітні")
-elif age <= 0:
-    print("Ви ще не народились")
-elif False:
-    print("I hear the voices in my head")
-else:
-    print("Ви неповнолітні")
+# Зчитування 10 слів з клавіатури
+words = [input(f"Введіть слово {i + 1}: ") for i in range(10)]
 
+# Сортування слів в алфавітному порядку
+sorted_words = sorted(words)
 
-print("Go next")
+# Зчитування 10 чисел з плаваючою зап'ятою з клавіатури
+numbers = [float(input(f"Введіть число {i + 1}: ")) for i in range(10)]
+
+# Сортування чисел з плаваючою зап'ятою за порядком зростання
+sorted_numbers = sorted(numbers)
+
+# Виведення результату
+print("Слова в алфавітному порядку:", sorted_words)
+print("Числа в порядку зростання:", sorted_numbers)
