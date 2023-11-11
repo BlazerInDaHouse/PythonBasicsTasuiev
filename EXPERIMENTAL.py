d50a8527a9b95c413af7a9d0e6ef40a0ddde269c
Umar Tasuiev <prmtzn@icloud.com>
2023-11-11 19:24:27 +0200

    EXPERIMENTAL.py

--- EXPERIMENTAL.py
+++ EXPERIMENTAL.py
@@ -1,1 +1,8 @@
-print("Hello world")
+age_str = input("Скільки вам років?")
+age = int(age_str)
+if age >= 18:
+    print("Ви повнолітні")
+elif age <= 0:
+    print("Ви ще не народились")
+else:
+    print("Ви неповнолітні")
